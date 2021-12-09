23812995cd7d793002e987a8653a776278a9497a
tricksterror <94676116+tricksterror@users.noreply.github.com>
2021-12-09 10:58:47 +0530

    commit

--- graph theory.cpp
+++ graph theory.cpp
@@ -14,3 +14,22 @@ for (int i = 0; i < n+1; ++i) {
     cout << graph[i][j] << " ";
   }cout << endl;
 }
+
+//adjacency list
+
+const int N = 1e3+10;
+vector<int> g[N];
+
+int n,m;
+cin >> n >> m;
+for (int i = 0; i < m; ++i) {
+  int v1, v2; cin >> v1 >> v2;
+  g[v1].push_back(v2); g[v2].push_back(v1);
+}
+for (int i = 1; i <= n; ++i) {
+  cout << i << "-->";
+  for (int x : g[i]) {
+    cout << x << " ";
+  }
+  cout << endl;
+}
