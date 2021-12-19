d7f0a424195486318250da84351282556d19646c
tricksterror <94676116+tricksterror@users.noreply.github.com>
2021-12-19 12:02:20 +0530

    commit

--- number theory.cpp
+++ number theory.cpp
@@ -35,18 +35,46 @@ int gcd(int a, int b) { // gcd of two numbers
 	if (a == 0) return b;
 	return gcd(b%a, a);
 }
-int binexprec(int a, int b) { //recursive binary exponentation.
+
+//recursive binary exponentation.
+int binexprec(int a, int b) { 
 	if (b == 0) return 1;
 	long long int ans = binexprec(a, b/2);
 	if (b & 1) return (a*((ans*ans)%M)) % M;
 	else return (ans*ans) % M;
 }
 
-int binexpitr(int a, int b) {//iterative binary exponentation
+//iterative binary exponentation
+int binexpitr(int a, int b) {
 	int ans = 1;
 	while (b != 0) {
 		if (b & 1) ans = (ans*1ll*a) % M;
 		a = (a*1ll*a) % M;
+		b >>= 1;
+	}
+	return ans;
+}
+
+const long long int M = 1e18+7;
+
+// if the value of M becomes greater than integer range like ll int
+//binary multiplication when we have to take mod of number greater than integer.
+//multiplication function if we have to take mod of two big numbers where mod is big
+
+int binmul(long long int a, long long int b) { 
+	int ans = 0;
+	while (b != 0) {
+		if ( b & 1) ans = (ans + a) % M;
+		a = (a + a) % M;
+		b >>= 1;
+	}
+	return ans;
+}
+int binexpitr(long long int a, long long int b) {
+	int ans = 1;
+	while (b != 0) {
+		if (b & 1) ans = binmul(ans, a);
+		a = binmul(a,a);
 		b >>= 1;
 	}
 	return ans;
