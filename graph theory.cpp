7ec3efaf2a3b90679ddf8191cd0291291cc339fc
tricksterror <94676116+tricksterror@users.noreply.github.com>
2021-12-26 09:30:33 +0530

    commit

--- graph theory.cpp
+++ graph theory.cpp
@@ -33,3 +33,40 @@ for (int i = 1; i <= n; ++i) {
   }
   cout << endl;
 }
+
+
+
+//dfs
+
+const int N = 1e3 + 10;
+
+vector<int> g[N];
+vector<int> vis(N);
+
+
+void dfs(int vertex) {
+        vis[vertex] = true;
+        for (int child : g[vertex]) {
+                if (vis[child]) continue;
+                dfs(child);
+       }
+}
+
+//bfs
+int level[N];
+void bfs(int source) {
+        queue<int> q;
+        q.push(source);
+        vis[source] = 1;
+        while (!q.empty()) {
+                int cur_v = q.front();
+                q.pop();
+                for (int child : g[cur_v]) {
+                        if (!vis[child]) {
+                                q.push(child);
+                                vis[child] = 1;
+                                level[child] = level[cur_v] + 1;
+                        }
+                }
+        }
+}
